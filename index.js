134a5845b80678d9dd796bba815721138c937522
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-14 04:34:19 +0000

    solved for nested arrays using recursion

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+  for (let element of array) {
+    if (element.length !== undefined) {
+      sum += sumItems(element)
+    } else {
+      sum += element;
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
